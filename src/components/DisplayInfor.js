9cc935b512bf4d9abdb53875acc4537d2a51f401
huysonny <huy26072003h@gmail.com>
2023-06-07 11:54:26 +0700

    'img'

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DisplayInfor.scss";
+import logo from "./../logo.svg";
 class DisplayInfor extends React.Component {
   state = {
     isShowListUser: true,
@@ -16,6 +17,7 @@ class DisplayInfor extends React.Component {
     //props => viết tắt properties
     return (
       <div className="display-infor-container">
+        <img src={logo} />
         <div>
           <span
             onClick={() => {
