fd7f5e9d60a6694be9e8c2dd921f98c227625e2b
huysonny <huy26072003h@gmail.com>
2023-06-07 17:03:59 +0700

    'lifecycle'

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,16 +2,39 @@ import React from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 class DisplayInfor extends React.Component {
-  state = {
-    isShowListUser: true,
-  };
+  constructor(props) {
+    console.log(">>> call constructor : 1");
+    super(props);
+    this.state = {
+      isShowListUser: true,
+    };
+  }
+
+  componentDidMount() {
+    console.log(">>> call me component did mount ");
+    setTimeout(() => {
+      document.title = "Thanh Huy";
+    }, 3000);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(">>> call me component did update", this.props, prevProps);
+    if (this.props.listUsers !== prevProps.listUsers) {
+      if (this.props.listUsers.length === 5) {
+        alert("you got 5 users");
+      }
+    }
+  }
+
   handleShowHide = () => {
     this.setState({
       isShowListUser: !this.state.isShowListUser,
     });
   };
+
   render() {
     // destructuring array /object
+    console.log(">>> call me render : ");
     const { listUsers } = this.props;
     // console.log(listUsers);
     //props => viết tắt properties
