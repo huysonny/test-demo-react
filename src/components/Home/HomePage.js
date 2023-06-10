220eb118e973a883b4dfff61fbc067944a547807
huysonny <huy26072003h@gmail.com>
2023-06-10 14:00:44 +0700

    ''

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,4 +1,11 @@
+import videoHomePage from "../../assets/video-homepage.mp4";
 const HomePage = (props) => {
-  return <div className="homepage-container">home page component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomePage} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 export default HomePage;
