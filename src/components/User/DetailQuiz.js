83c07a9debfb625bd0dfa34e9b466ae618f33734
huysonny <huy26072003h@gmail.com>
2023-06-15 18:17:33 +0700

    ''

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,17 +1,70 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../services/apiServices";
+import _ from "lodash";
+import "./DetailQuiz.scss";
 const DetalQuiz = (props) => {
   const params = useParams();
   const quizId = params.id;
+  const location = useLocation();
+  console.log(location);
   useEffect(() => {
     fetchQuestions();
   }, [quizId]);
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizId);
     console.log(">>> check question : ", res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `id` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+
+          return { questionId: key, answers, questionDescription, image };
+        })
+        .value();
+      console.log(data);
+    }
   };
   console.log("check params: ", params);
-  return <div className="detail-quiz-container">DetalQuiz</div>;
+  return (
+    <div className="detail-quiz-container">
+      <div className="left-content">
+        <div className="title">
+          Quiz {quizId}: {location?.state?.quizTitle}
+        </div>
+        <hr />
+        <div className="q-body">
+          <img />
+        </div>
+        <div className="q-content">
+          <div className="question">Question 1 : how are you doing ?</div>
+          <div className="answer">
+            <div className="a-child">A. asdasdsa</div>
+            <div className="a-child">B. adsfsdafsdafasdfsd</div>
+            <div className="a-child">C. asdfdsfsdafdas</div>
+          </div>
+        </div>
+        <div className="footer">
+          <button className="btn btn-secondary">Prev</button>
+          <button className="btn btn-primary ">Next</button>
+        </div>
+      </div>
+      <div className="right-content">count down</div>
+    </div>
+  );
 };
 export default DetalQuiz;
--- src/components/User/ListQuiz.js
+++ src/components/User/ListQuiz.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { getQuizByUser } from "../services/apiServices";
 import "./ListQuiz.scss";
+
 import { useNavigate } from "react-router-dom";
 const ListQuiz = () => {
   const navigate = useNavigate();
@@ -35,9 +36,12 @@ const ListQuiz = () => {
                 <h5 className="card-title">Quiz {index + 1}</h5>
                 <p className="card-text">{quiz.description}</p>
                 <button
-                  href="#"
                   className="btn btn-primary"
-                  onClick={() => navigate(`/quiz/${quiz.id}`)}
+                  onClick={() =>
+                    navigate(`/quiz/${quiz.id}`, {
+                      state: { quizTitle: quiz.description },
+                    })
+                  }
                 >
                   Start Now
                 </button>
