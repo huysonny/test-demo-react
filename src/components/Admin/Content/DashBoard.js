30c8a4cbab9b7e4b420930fac44610330f502b7a
huysonny <huy26072003h@gmail.com>
2023-06-23 15:37:57 +0700

    ''

--- src/components/Admin/Content/DashBoard.js
+++ src/components/Admin/Content/DashBoard.js
@@ -1,65 +1,125 @@
-import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from "recharts";
+import {
+  ResponsiveContainer,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  XAxis,
+  YAxis,
+} from "recharts";
 import "./DashBoard.scss";
 import { Tooltip } from "react-bootstrap";
+import { getOverview } from "../../services/apiServices";
+import { useEffect, useState } from "react";
 const DashBoard = (props) => {
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-    },
-  ];
+  const [dataOverView, setDataOverView] = useState([]);
 
+  const [dataChart, setDataChart] = useState([]);
+
+  useEffect(() => {
+    fetchDataOverView();
+  }, []);
+
+  const fetchDataOverView = async () => {
+    let res = await getOverview();
+    if (res && res.EC === 0) {
+      setDataOverView(res.DT);
+      //process chart data
+      let Qz = 0;
+      let Qs = 0;
+      let As = 0;
+      Qz = res?.DT?.others?.countQuiz ?? 0;
+      Qs = res?.DT?.others?.countQuestions ?? 0;
+      As = res?.DT?.others?.countAnswers ?? 0;
+      const data = [
+        {
+          name: "Quizzes",
+          Qz: Qz,
+        },
+        {
+          name: "Questions",
+          Qs: Qs,
+        },
+        {
+          name: "Answers",
+          As: As,
+        },
+      ];
+      setDataChart(data);
+    }
+    console.log(">>> check res : ", res);
+  };
+
+  console.log(">>> dataOverView:  ", dataOverView);
   return (
     <div className="dashboard-Container">
       <div className="title"> Analytics dashboard</div>
       <div className="content">
         <div className="c-left">
-          <div className="child">Total users</div>
-          <div className="child">Total Quiz</div>
-          <div className="child">Total Question</div>
-          <div className="child">Total Answers</div>
+          <div className="child">
+            <span className="text-1">Total users</span>
+            <span className="text-2">
+              {dataOverView &&
+              dataOverView.users &&
+              dataOverView.users.total ? (
+                <>{dataOverView.users.total}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span className="text-1">Total Quizzes</span>
+            <span className="text-2">
+              {" "}
+              {dataOverView &&
+              dataOverView.others &&
+              dataOverView.others.countQuiz ? (
+                <>{dataOverView.others.countQuiz}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span className="text-1">Total Questions</span>
+            <span className="text-2">
+              {" "}
+              {dataOverView &&
+              dataOverView.others &&
+              dataOverView.others.countQuestions ? (
+                <>{dataOverView.others.countQuestions}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
+          <div className="child">
+            <span className="text-1">Total Answers</span>
+            <span className="text-2">
+              {dataOverView &&
+              dataOverView.others &&
+              dataOverView.others.countAnswers ? (
+                <>{dataOverView.others.countAnswers}</>
+              ) : (
+                <>0</>
+              )}
+            </span>
+          </div>
         </div>
         <div className="c-right">
-          <BarChart width={500} height={300} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
-          </BarChart>
+          <ResponsiveContainer width={"95%"} height={"100%"}>
+            <BarChart data={dataChart}>
+              {/* <CartesianGrid strokeDasharray="3 3" /> */}
+              <XAxis dataKey="name" />
+              {/* <YAxis /> */}
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="Qz" fill="#8884d8" />
+              <Bar dataKey="Qs" fill="#82ca9d" />
+              <Bar dataKey="As" fill="#fcb12a" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
--- src/components/services/apiServices.js
+++ src/components/services/apiServices.js
@@ -98,6 +98,9 @@ const postUpsertQA = (data) => {
 const logout = (email, refresh_token) => {
   return axios.post("api/v1/logout", { email, refresh_token });
 };
+const getOverview = () => {
+  return axios.get("api/v1/overview");
+};
 export {
   postCreateNewUser,
   getAllUsers,
@@ -119,4 +122,5 @@ export {
   getQuizWithQA,
   postUpsertQA,
   logout,
+  getOverview,
 };
